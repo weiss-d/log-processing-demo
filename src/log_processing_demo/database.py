3715ce04791fd4ff73939b199576c155fb97f08b
Simon Weiss <wonder@simonweiss.space>
2021-03-11 13:20:51 +0300

    Added logging with decorator. Added docstrings.

--- src/log_processing_demo/database.py
+++ src/log_processing_demo/database.py
@@ -1,3 +1,7 @@
+"""
+Database interaction module.
+"""
+import functools
 import logging
 import sqlite3
 from datetime import datetime, timedelta
@@ -6,20 +10,84 @@ from typing import Dict, List, Optional, Tuple, Union
 logger = logging.getLogger(__name__)
 
 
-class Database:
-    def __init__(self, db_uri: str) -> None:
-        try:
-            self.connection: sqlite3.Connection = sqlite3.connect(
-                db_uri, detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES
-            )
-            self.connection.row_factory = sqlite3.Row
-            self.cursor: sqlite3.Cursor = self.connection.cursor()
-            logger.info("Successfuly connected to database at %s", db_uri)
-        except Exception as error:
-            logger.error("Database connection error: %s", str(error))
-            raise
+def db_logging(log_statement: str):
+    """Decorator for logging database operations.
 
+    Parameters
+    ----------
+    log_statement : str
+        Log statement that describes DB operation.
+    """
+
+    def decorator(function):
+        @functools.wraps(function)
+        def wrapper(*args, **kwargs):
+            try:
+                result_or_none = function(*args, **kwargs)
+                logger.info("Success: %s", log_statement)
+                return result_or_none
+            except Exception as error:
+                logger.error("Error: %s.\n%s", log_statement, str(error))
+                raise
+
+        return wrapper
+
+    return decorator
+
+
+class Database:
+    """Class to update and read from a DB."""
+
+    def __init__(self, db_uri: str) -> None:
+        """Constructor.
+
+        Parameters
+        ----------
+        db_uri : str
+            *.db file path or ':memory:'.
+
+        Returns
+        -------
+        None
+
+        """
+        self.db_uri = db_uri
+        self._connect_to_db(self.db_uri)
+
+    @db_logging("connect to database")
+    def _connect_to_db(self, db_uri: str) -> None:
+        """Initializes SQLite connection with options to handle timestamps.
+
+        Parameters
+        ----------
+        db_uri : str
+            *.db file path or ':memory:'.
+
+        Returns
+        -------
+        None
+
+        """
+        self.connection: sqlite3.Connection = sqlite3.connect(
+            db_uri, detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES
+        )
+        self.connection.row_factory = sqlite3.Row
+        self.cursor: sqlite3.Cursor = self.connection.cursor()
+
+    @db_logging("update LOG data")
     def update(self, message_list: List[Dict[str, Union[datetime, str]]]) -> None:
+        """Update database with logs fetched by LogReceiver.
+
+        Parameters
+        ----------
+        message_list : List[Dict[str, Union[datetime, str]]]
+            Output of call to LogReceiver object.
+
+        Returns
+        -------
+        None
+
+        """
         self.cursor.execute(
             """
             CREATE TABLE IF NOT EXISTS log_messages (
@@ -51,9 +119,25 @@ class Database:
             )
         self.connection.commit()
 
+    @db_logging("retrieve LOG data")
     def read(
         self, date: str, time_interval: Optional[Tuple[str]] = None
     ) -> List[Dict[str, Union[datetime, str]]]:
+        """Read log messages for desired date from database, optionally filtering by time.
+
+        Parameters
+        ----------
+        date : str
+            LOG date. Format: YYYY-MM-DD
+        time_interval : Optional[Tuple[str]]
+            Tuple of desired time boundaries. Time format: HH:MM:SS
+
+        Returns
+        -------
+        List[Dict[str, Union[datetime, str]]]
+            Structure is identical to LogReceiver output.
+
+        """
         if time_interval:
             time_boundaries: Tuple[datetime] = (
                 datetime.fromisoformat(f"{date}T{time_interval[0]}"),
